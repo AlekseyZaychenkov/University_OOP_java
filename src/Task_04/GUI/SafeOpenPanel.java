a22bed2472d1e27ec6a66d410a308191abd65299
AlekseyZaychenkov <alexzdes.1989@yandex.ru>
2019-12-13 19:17:20 +0300

    Task_04 finished

--- src/Task_04/GUI/SafeOpenPanel.java
+++ src/Task_04/GUI/SafeOpenPanel.java
@@ -1,12 +1,12 @@
 package Task_04.GUI;
 
+import javax.activation.UnsupportedDataTypeException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.InterruptedByTimeoutException;
 import java.util.Scanner;
 
 /**
@@ -42,8 +42,25 @@ public class SafeOpenPanel extends JPanel {
                     try ( FileWriter fw = new FileWriter(fc.getSelectedFile()) ) {
                         System.out.println("try: "+GUI_Task_04.textField.getText().toString());
                         fw.write(GUI_Task_04.textField.getText().toString());
-                    } catch ( IOException ex ) {
-                        System.out.println(ex);
+                    } catch (FileNotFoundException fnf){
+                        System.out.println(fnf);
+                        JOptionPane.showMessageDialog(new JFrame(), "Отказанно в доступе!", "Warning",
+                                JOptionPane.WARNING_MESSAGE);
+                    } catch (InterruptedByTimeoutException ibt){
+                        System.out.println(ibt);
+                        JOptionPane.showMessageDialog(new JFrame(), "Ошибка записи! Превышено время ожидания!", "Warning",
+                                JOptionPane.WARNING_MESSAGE);
+
+                    } catch (UnsupportedDataTypeException | UnsupportedEncodingException udt){
+                        System.out.println(udt);
+                        JOptionPane.showMessageDialog(new JFrame(), "Неразрешенный символ для записи!", "Warning",
+                                JOptionPane.WARNING_MESSAGE);
+
+
+                    } catch (IOException ex2) {
+                        System.out.println(ex2);
+                        JOptionPane.showMessageDialog(new JFrame(), "Неизвестная ошибка при открытии файла!", "Warning",
+                                JOptionPane.WARNING_MESSAGE);
                     }
                 }
 
@@ -68,15 +85,17 @@ public class SafeOpenPanel extends JPanel {
                                 //System.out.println("while"+sc.nextLine());
                                 GUI_Task_04.myStringBuilder.append(sc.nextLine() + '\n');
                             }
-                        } catch (IllegalArgumentException iae){
-                            JOptionPane.showMessageDialog(new JFrame(), "Неизвестный формат файла!", "Warning",
+                        } catch (FileNotFoundException fnf){
+                            System.out.println(fnf);
+                            JOptionPane.showMessageDialog(new JFrame(), "Отказанно в доступе!", "Warning",
                                     JOptionPane.WARNING_MESSAGE);
-                        } catch (IOException ex2) {
-                            JOptionPane.showMessageDialog(new JFrame(), "Неизвестная ошибка при открытии файла!", "Warning",
+                        } catch (IllegalArgumentException iae){
+                            System.out.println(iae);
+                            JOptionPane.showMessageDialog(new JFrame(), "Неизвестный формат файла!", "Warning",
                                     JOptionPane.WARNING_MESSAGE);
                         }
                     }
-                    System.out.println("textField: " + GUI_Task_04.myStringBuilder.toString());
+                    //System.out.println("textField: " + GUI_Task_04.myStringBuilder.toString());
                     GUI_Task_04.textField.setText(GUI_Task_04.myStringBuilder.toString());
                 }
             }
